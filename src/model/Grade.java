d144c0b0efc86ab0098271f2f95b74a5d0c540c9
macmuzyka <raweshau@gmail.com>
2021-03-13 17:35:15 +0100

    grade POJO

---
+++ src/model/Grade.java
@@ -0,0 +1,28 @@
+package model;
+
+/**
+ * Created by Raweshau
+ * on 22.10.2020
+ */
+
+public class Grade {
+//
+    private int ID;
+    private int value;
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
