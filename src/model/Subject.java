de3c5028dbc9364640f7a15af120fbe16d9b422e
macmuzyka <raweshau@gmail.com>
2021-03-13 17:39:28 +0100

    subject POJO

---
+++ src/model/Subject.java
@@ -0,0 +1,30 @@
+package model;
+
+/**
+ * Created by Raweshau
+ * on 22.10.2020
+ */
+
+public class Subject {
+
+    //
+
+    private int ID;
+    private String subjectName;
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getSubjectName() {
+        return subjectName;
+    }
+
+    public void setSubjectName(String subjectName) {
+        this.subjectName = subjectName;
+    }
+}
