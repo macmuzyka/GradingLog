d8fd6ca1dfde705bd21526b70f5b72fc6dbb4521
macmuzyka <raweshau@gmail.com>
2021-03-13 17:38:42 +0100

    student POJO

---
+++ src/model/Student.java
@@ -0,0 +1,28 @@
+package model;
+
+/**
+ * Created by Raweshau
+ * on 22.10.2020
+ */
+
+public class Student {
+//
+    private int ID;
+    private String studentName;
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+}
